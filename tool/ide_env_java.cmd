363431be398dec1c444708fb0ec62875128e64c8
Ikki <ikkiDay@163.com>
2021-03-18 17:54:00 +0800

    Update ide_env_java.cmd

--- tool/ide_env_java.cmd
+++ tool/ide_env_java.cmd
@@ -1,4 +1,12 @@
+echo "set JAVA env ..."
 setx JAVA_HOME "D:/lang/java"
-setx NODE_HOME "D:/lang/node"
 setx PATH "%PATH%;%JAVA_HOME%/bin"
+
+echo "set NODE env ..."
+setx NODE_HOME "D:/lang/node"
 setx PATH "%PATH%;%NODE_HOME/%"
+
+echo "set RUST env ..."
+setx RUST_HOME "D:/lang/rust"
+setx CARGO_HOME "D:/lang/cargo"
+
